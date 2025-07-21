fd93d6f386f1c4b29c0931eb8db269b751feeee4
AvinashTech99 <avinashkharpude99@gmail.com>
2025-07-22 05:24:45 +0530

    Problem 2 nd solved

---
+++ ProblemSolving/ListNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class ListNode
+    {
+        public int val { get; set; }
+        public ListNode next {  get; set; }
+        public ListNode(int val=0,ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -2,5 +2,48 @@
 using ProblemSolving;
 
 Console.WriteLine("Hello, World!");
-_1TwoSum twoSum = new _1TwoSum();
-twoSum.Run();
+//_1TwoSum twoSum = new _1TwoSum();
+//twoSum.Run();
+
+/*  Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+    Example 2:
+ */
+
+ListNode l1 = new ListNode()
+{
+    val=2,
+    next = new ListNode()
+    {
+        val = 4,
+        next = new ListNode()
+        {
+            val = 3,
+            next = null
+        }
+    }
+};
+
+ListNode l2 = new ListNode()
+{
+    val = 5,
+    next = new ListNode()
+    {
+        val = 6,
+        next = new ListNode()
+        {
+            val = 4,
+            next = null
+        }
+    }
+};
+
+_2AddTwoNumbers _2AddTwoNumbers = new _2AddTwoNumbers();
+ListNode result = _2AddTwoNumbers.AddTwoNumbers(l1 , l2);
+
+while(result != null)
+{
+    Console.WriteLine(result.val);
+    result = result.next;
+}
