80841cad3bb63ac99367a5ab282032c98f4c58f7
LENOVO <LENOVO@DESKTOP-MQL7H2Q>
2025-11-23 22:11:55 +0530

    Question 5 try to solved

---
+++ ProblemSolving/Solutions/Q_5/LongestPalindrome_Wrong.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Solutions.Q_5
+{
+    public class LongestPalindrome_Wrong
+    {
+        //Wrong Answer
+        public string Run(string s)
+        {
+            //ccc
+            int pointer = 0, traversal = 1;
+            int[] substring = new int[2];
+            if(s.Length <= 1)
+            {
+                return s;
+            }
+            if(s.Length == 2 && s[0] != s[1])
+            {
+                return s.Substring(0, 1);
+            }
+            while (pointer < s.Length-1)
+            {
+                if (substring[0] + substring[1] < traversal - pointer)
+                {
+                    if(pointer-traversal+1  <= 3)
+                    {
+                        if (s[pointer] == s[traversal])
+                        {
+                            substring[0] = pointer;
+                            substring[1] = traversal;
+                        }
+                    }
+                    else
+                    {
+                        string temp = s.Substring(pointer,traversal);
+                        if(temp == temp.Reverse().ToString())
+                        {
+                            substring[0] = pointer;
+                            substring[1] = traversal;
+                        }
+                    }
+                }
+                if (traversal == s.Length-1)
+                {
+                    pointer++;
+                    traversal = pointer + 1;
+                    continue;
+                }
+                else
+                {
+                    traversal++;
+                }
+            }
+            return s.Substring(substring[0], substring[1]);
+        }
+    }
+}
---
+++ ProblemSolving/Study/TimeComplexity.cs
@@ -0,0 +1,72 @@
+﻿/*
+ 
+Time complexity tells us how the running time increases as the input size increases.
+
+ Time complexity is measured using asymptotic notations, primarily Big O notation, to describe how an algorithm's runtime grows in relation to its input size.
+
+Other notations like Omega (\(\Omega \)) and Theta (\(\Theta \)) are also used to denote the best-case and average-case scenarios, respectively. 
+
+--------------- Setps ------------------------------------------------
+1 >> Count how many times each part of the code runs
+loops, recursions, iterated operations
+Example:
+    for (int i = 0; i < n; i++)
+    {
+    }
+    this loop runs n times -> O(n) 
+
+
+2>> Ignore constant-time operations
+
+    simple operation like addition, subtraction, assignment, comparison
+    All of these are O(1).
+ 
+
+3>> Focus on loops
+
+    loops tell you the dominant time
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+      {
+      }
+    
+    Inner loops run n tomes
+    Outer loop runs n times
+
+    Total operations = n * n = n^2 -> O(n^2)
+
+4 >> Add time  when loops are sequential
+    for (int i = 0; i < n; i++)
+    {
+    }
+    for (int j = 0; j < m; j++)
+    {
+    }
+    Total operations = n + m -> O(n + m) 
+
+    for (int i = 0; i < n; i++)
+    {
+    }
+    for (int j = 0; j < n; j++)
+    {
+    }
+    Total operations = n + n = 2n -> O(n)
+    
+
+5 >> Multiply time when loops are nested
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+      {
+      }
+    
+    Total operations = n * m -> O(n*m)
+ 
+6 >> Drop non-dominant terms and constants
+    O(n^2 + n) -> O(n^2)
+    O(n + log n) -> O(n)
+    O(n^2 + n log n) -> O(n^2) 
+    
+    O(2n + 5) → O(n)
+    O(n² + n) → O(n²)
+
+ */
